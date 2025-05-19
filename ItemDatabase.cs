deee5a368800b0204fa82c14e89ad5ca84a02875
AhmetBakIrkaya <ahmetbkaya2212@gmail.com>
2025-05-19 15:51:17 +0300

    Items that can affect each stat separately for each pet

--- ItemDatabase.cs
+++ ItemDatabase.cs
@@ -4,15 +4,189 @@ public static class ItemDatabase
 {
     public static List<Item> AllItems = new List<Item>
     {
-        new Item {
-            Name = "Kibble", Type = ItemType.Food,
-            CompatibleWith = new List<PetType> { PetType.Dog, PetType.Cat },
-            AffectedStat = PetStat.Hunger, EffectAmount = 15, Duration = 2.5f
+        // DOG
+        new Item
+        {
+            Name = "Dog Bone",
+            AffectedStat = PetStat.Fun,
+            EffectAmount = 20,
+            Duration = 2,
+            CompatibleWith = new List<PetType> { PetType.Dog }
         },
-        new Item {
-            Name = "Ball", Type = ItemType.Toy,
-            CompatibleWith = new List<PetType> { PetType.Dog, PetType.Cat },
-            AffectedStat = PetStat.Fun, EffectAmount = 10, Duration = 2.0f
+        new Item
+        {
+            Name = "Dog Food",
+            AffectedStat = PetStat.Hunger,
+            EffectAmount = 20,
+            Duration = 2,
+            CompatibleWith = new List<PetType> { PetType.Dog }
+        },
+        new Item
+        {
+            Name = "Dog Bed",
+            AffectedStat = PetStat.Sleep,
+            EffectAmount = 15,
+            Duration = 3,
+            CompatibleWith = new List<PetType> { PetType.Dog }
+        },
+
+        // CAT
+        new Item
+        {
+            Name = "Cat Toy",
+            AffectedStat = PetStat.Fun,
+            EffectAmount = 15,
+            Duration = 2,
+            CompatibleWith = new List<PetType> { PetType.Cat }
+        },
+        new Item
+        {
+            Name = "Cat Food",
+            AffectedStat = PetStat.Hunger,
+            EffectAmount = 15,
+            Duration = 2,
+            CompatibleWith = new List<PetType> { PetType.Cat }
+        },
+        new Item
+        {
+            Name = "Cat Pillow",
+            AffectedStat = PetStat.Sleep,
+            EffectAmount = 20,
+            Duration = 3,
+            CompatibleWith = new List<PetType> { PetType.Cat }
+        },
+
+        // TURTLE
+        new Item
+        {
+            Name = "Turtle Heater Stone",
+            AffectedStat = PetStat.Sleep,
+            EffectAmount = 25,
+            Duration = 3,
+            CompatibleWith = new List<PetType> { PetType.Turtle }
+        },
+        new Item
+        {
+            Name = "Turtle Treat",
+            AffectedStat = PetStat.Hunger,
+            EffectAmount = 20,
+            Duration = 2,
+            CompatibleWith = new List<PetType> { PetType.Turtle }
+        },
+        new Item
+        {
+            Name = "Floating Log Toy",
+            AffectedStat = PetStat.Fun,
+            EffectAmount = 10,
+            Duration = 2,
+            CompatibleWith = new List<PetType> { PetType.Turtle }
+        },
+
+        // BIRD
+        new Item
+        {
+            Name = "Bird Treat",
+            AffectedStat = PetStat.Hunger,
+            EffectAmount = 10,
+            Duration = 1,
+            CompatibleWith = new List<PetType> { PetType.Bird }
+        },
+        new Item
+        {
+            Name = "Bird Swing",
+            AffectedStat = PetStat.Fun,
+            EffectAmount = 15,
+            Duration = 2,
+            CompatibleWith = new List<PetType> { PetType.Bird }
+        },
+        new Item
+        {
+            Name = "Bird Nest",
+            AffectedStat = PetStat.Sleep,
+            EffectAmount = 20,
+            Duration = 3,
+            CompatibleWith = new List<PetType> { PetType.Bird }
+        },
+
+        // FISH
+        new Item
+        {
+            Name = "Fish Flakes",
+            AffectedStat = PetStat.Hunger,
+            EffectAmount = 15,
+            Duration = 2,
+            CompatibleWith = new List<PetType> { PetType.Fish }
+        },
+        new Item
+        {
+            Name = "Bubble Cave",
+            AffectedStat = PetStat.Sleep,
+            EffectAmount = 15,
+            Duration = 3,
+            CompatibleWith = new List<PetType> { PetType.Fish }
+        },
+        new Item
+        {
+            Name = "Spinning Water Toy",
+            AffectedStat = PetStat.Fun,
+            EffectAmount = 10,
+            Duration = 2,
+            CompatibleWith = new List<PetType> { PetType.Fish }
+        },
+
+        // RABBIT
+        new Item
+        {
+            Name = "Rabbit Carrot Snack",
+            AffectedStat = PetStat.Fun,
+            EffectAmount = 15,
+            Duration = 2,
+            CompatibleWith = new List<PetType> { PetType.Rabbit }
+        },
+        new Item
+        {
+            Name = "Rabbit Hay",
+            AffectedStat = PetStat.Hunger,
+            EffectAmount = 20,
+            Duration = 2,
+            CompatibleWith = new List<PetType> { PetType.Rabbit }
+        },
+        new Item
+        {
+            Name = "Rabbit Burrow Nest",
+            AffectedStat = PetStat.Sleep,
+            EffectAmount = 15,
+            Duration = 3,
+            CompatibleWith = new List<PetType> { PetType.Rabbit }
+        },
+        
+        // UNIVERSAL ITEMS
+        
+        new Item
+        {
+            Name = "Talk to your Pet",
+            AffectedStat = PetStat.Fun,
+            EffectAmount = 15,
+            Duration = 2,
+            CompatibleWith = new List<PetType> 
+            { 
+                PetType.Dog, PetType.Cat, PetType.Turtle,
+                PetType.Bird, PetType.Fish, PetType.Rabbit
+                
+            }
+        },
+        new Item
+        {
+            Name = "Mystery Pet Snack",
+            AffectedStat = PetStat.Hunger,
+            EffectAmount = 15,
+            Duration = 2,
+            CompatibleWith = new List<PetType>
+            {
+                PetType.Dog, PetType.Cat, PetType.Turtle,
+                PetType.Bird, PetType.Fish, PetType.Rabbit
+            }
         }
     };
 }
+
--- PetType.cs
+++ PetType.cs
@@ -2,8 +2,8 @@ public enum PetType
 {
     Dog,
     Cat,
+    Turtle,
     Bird,
     Fish,
-    Rabbit,
-    Turtle
+    Rabbit
 }
